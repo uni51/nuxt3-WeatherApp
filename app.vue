46bff1a841bce90ad11a8389e7ba59b96f6d131c
Koichi.Shimomura <unieye51@gmail.com>
2022-07-25 13:07:56 +0900

    7-42 Writing the HTML Structure

--- app.vue
+++ app.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="h-screen relative overflow-hidden">
+    <img />
+    <div class="absolute w-full h-full top-0 overlay" />
+    <div class="absolute w-full h-full top-0 p-48">
+      <div class="flex justify-between">
+        <div>
+          <h1 class="text-7xl text-white">Tronto</h1>
+          <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
+          <img class="w-56 icon" />
+        </div>
+        <div>
+          <p class="text-9xl text-white font-extralight">53°</p>
+        </div>
+      </div>
+      <div class="mt-20">
+        <input type="text" class="w-1/2 h-10" placeholder="Search a city..." />
+        <button class="bg-sky-400 w-20 text-white h-10">Search</button>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.overlay {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.icon {
+  margin-left: -2.75rem;
+  margin-top: -2.75rem;
+}
+</style>
