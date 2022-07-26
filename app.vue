912ff4bbb20ddee24cf35751c53d500879a86cb5
Koichi.Shimomura <unieye51@gmail.com>
2022-07-26 15:53:05 +0900

    7-44 The useFetch Composable

--- app.vue
+++ app.vue
@@ -1,16 +1,43 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+
+const { data: city, error } = useAsyncData("city", async () => {
+  let response;
+  try {
+    response = await $fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=Tokyo`,
+      {
+        params: {
+          units: "metric",
+          appid: config.WEATHER_APP_SECRET,
+        },
+      }
+    );
+  } catch (e) {}
+
+  return response;
+});
+</script>
+
 <template>
   <div class="h-screen relative overflow-hidden">
+    {{ city }}
     <img />
     <div class="absolute w-full h-full top-0 overlay" />
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
         <div>
-          <h1 class="text-7xl text-white">Tronto</h1>
+          <h1 class="text-7xl text-white">{{ city.name }}</h1>
           <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
-          <img class="w-56 icon" />
+          <img
+            :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`"
+            class="w-56 icon"
+          />
         </div>
         <div>
-          <p class="text-9xl text-white font-extralight">53°</p>
+          <p class="text-9xl text-white font-extralight">
+            {{ city.main.temp }}°
+          </p>
         </div>
       </div>
       <div class="mt-20">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  publicRuntimeConfig: {
+    WEATHER_APP_SECRET: process.env.WEATHER_APP_SECRET,
+  },
 	css: ["~/assets/css/tailwind.css"],
 	build: {
 		postcss: {
