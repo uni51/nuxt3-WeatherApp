70bd417fa9fe5f222eb66c69f60f220df80e16f4
Koichi.Shimomura <unieye51@gmail.com>
2022-07-26 16:40:39 +0900

    8-51 Error Handling

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+const cookie = useCookie("city");
 const config = useRuntimeConfig();
 
-const search = ref("Tronto");
+if (!cookie.value) cookie.value = "Toronto";
+
+const search = ref(cookie.value);
 const input = ref("");
 const background = ref("");
 
@@ -20,7 +23,10 @@ const { data: city, error } = useAsyncData(
         }
       );
 
+      cookie.value = search.value;
+
       const temp = response.main.temp;
+
       if (temp <= -10) {
         background.value =
           "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
@@ -43,23 +49,34 @@ const { data: city, error } = useAsyncData(
   }
 );
 
+let today: string | Date = new Date();
+today = today.toLocaleDateString("en-US", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+});
+
 const handleClick = () => {
   const formatedSearch = input.value.trim().split(" ").join("+");
   search.value = formatedSearch;
   input.value = "";
 };
+
+const goBack = () => {
+  search.value = cookie.value;
+};
 </script>
 
 <template>
-  <div class="h-screen relative overflow-hidden">
-    {{ input }}
+  <div v-if="city" class="h-screen relative overflow-hidden">
     <img :src="background" />
     <div class="absolute w-full h-full top-0 overlay" />
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
         <div>
           <h1 class="text-7xl text-white">{{ city.name }}</h1>
-          <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
+          <p class="font-extralight text-2xl mt-2 text-white">{{ today }}</p>
           <img
             :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`"
             class="w-56 icon"
@@ -83,6 +100,12 @@ const handleClick = () => {
         </button>
       </div>
     </div>
+  </div>
+  <div v-else class="p-10">
+    <h1 class="text-7xl">Oops, we can't find that city</h1>
+    <button class="mt-5 bg-sky-400 px-10 w-50 text-white h-10" @click="goBack">
+      Go Back
+    </button>
   </div>
 </template>
 
