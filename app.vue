144de39a6bc5caf713c98352b20712c04193004b
Koichi.Shimomura <unieye51@gmail.com>
2022-07-26 16:13:09 +0900

    4-47 Refetching Data with useAsyncData

--- app.vue
+++ app.vue
@@ -1,28 +1,59 @@
 <script setup lang="ts">
 const config = useRuntimeConfig();
 
-const { data: city, error } = useAsyncData("city", async () => {
-  let response;
-  try {
-    response = await $fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=Tokyo`,
-      {
-        params: {
-          units: "metric",
-          appid: config.WEATHER_APP_SECRET,
-        },
-      }
-    );
-  } catch (e) {}
+const search = ref("Tronto");
+const input = ref("");
+const background = ref("");
 
-  return response;
-});
+const { data: city, error } = useAsyncData(
+  "city",
+  async () => {
+    let response;
+    try {
+      response = await $fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search.value}`,
+        {
+          params: {
+            units: "metric",
+            appid: config.WEATHER_APP_SECRET,
+          },
+        }
+      );
+
+      const temp = response.main.temp;
+      if (temp <= -10) {
+        background.value =
+          "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > -10 && temp <= 0) {
+        background.value =
+          "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > 0 && temp <= 10) {
+        background.value =
+          "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
+      } else {
+        background.value =
+          "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+      }
+    } catch (e) {}
+
+    return response;
+  },
+  {
+    watch: [search],
+  }
+);
+
+const handleClick = () => {
+  const formatedSearch = input.value.trim().split(" ").join("+");
+  search.value = formatedSearch;
+  input.value = "";
+};
 </script>
 
 <template>
   <div class="h-screen relative overflow-hidden">
-    {{ city }}
-    <img />
+    {{ input }}
+    <img :src="background" />
     <div class="absolute w-full h-full top-0 overlay" />
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
@@ -41,8 +72,15 @@ const { data: city, error } = useAsyncData("city", async () => {
         </div>
       </div>
       <div class="mt-20">
-        <input type="text" class="w-1/2 h-10" placeholder="Search a city..." />
-        <button class="bg-sky-400 w-20 text-white h-10">Search</button>
+        <input
+          type="text"
+          class="w-1/2 h-10"
+          placeholder="Search a city..."
+          v-model="input"
+        />
+        <button class="bg-sky-400 w-20 text-white h-10" @click="handleClick">
+          Search
+        </button>
       </div>
     </div>
   </div>
